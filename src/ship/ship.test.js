b13cfc73cda90d6c516a073be120ef8227abc623
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-23 08:16:58 -0400

    Writing tests for the ship factory function

---
+++ src/ship/ship.test.js
@@ -0,0 +1,22 @@
+const createShip = require('./ship')
+
+/* eslint-disable no-undef */
+describe('ship factory function', () => {
+    test('ship length method', () => {
+        expect(createShip(3).length() === 3)
+    })
+    test('damaging the ship using the hit() method', () => {
+        const ship = createShip(5)
+        ship.hit(1)
+        expect(ship.getStatus() === ['hit', 'undamaged', 'undamaged', 'undamaged', 'undamaged'])
+    })
+    test('checking if the ship has sunk without having gotten hit', () => {
+        expect(createShip(2).isSunk() === false)
+    })
+    test('checking if the ship sinks when all positions are hit', () => {
+        const ship = createShip(2)
+        ship.hit(1)
+        ship.hit(2)
+        expect(ship.isSunk() === true)
+    })
+})
