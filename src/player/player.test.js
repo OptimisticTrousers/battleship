cb2159c52d401a3f761008453e6d9df4f629e90f
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-24 09:59:27 -0400

    Refactoring tests

--- src/player/player.test.js
+++ src/player/player.test.js
@@ -2,42 +2,33 @@
 const createPlayer = require('./player')
 
 describe('player factory function', () => {
+        const computer = createPlayer()
+        const player = createPlayer()
     test('testing if the player can shoot the enemy', () => {
-        player.attack('A', 3, computer)
-        expect(computer.getBoard().hasLastAttackHitShip('A', 3)).toBe(true)
+        player.attack('A', 2, computer)
+        expect(computer.getBoard().hasLastAttackHitShip('A', 2)).toBe(true)
     })
     test('testing if the player missed the enemy', () => {
-        const computer = createPlayer()
-        const player = createPlayer()
-        player.attack('A', 1, computer)
-        expect(computer.getBoard().hasLastAttackHitShip('A', 1)).toBe(false)
+        player.attack('A', 7, computer)
+        expect(computer.getBoard().hasLastAttackHitShip('A', 7)).toBe(false)
     })
     test('testing if the computer can shoot the player', () => {
-        const computer = createPlayer()
-        const player = createPlayer()
-        computer.attack('A', 3, player)
-        expect(computer.getBoard().hasLastAttackHitShip('A', 3)).toBe(true)
+        computer.attack('B', 9, player)
+        expect(computer.getBoard().hasLastAttackHitShip('B', 9)).toBe(true)
     })
     test('testing if the computer missed the player', () => {
-        const computer = createPlayer()
-        const player = createPlayer()
         computer.attack('A', 1, player)
         expect(computer.getBoard().hasLastAttackHitShip('A', 1)).toBe(false)
     })
     test('testing if the player can shoot a spot on the gameboard that has already been shot', () => {
-        const computer = createPlayer()
-        const player = createPlayer()
-        player.attack('A', 1, computer)
-        expect(player.attack('A', 1, computer)).toBe('You have already hit this spot!')
+        player.attack('J', 9, computer)
+        expect(player.attack('J', 9, computer)).toBe('You have already hit this spot!')
     })
     test('testing if the computer can shoot a spot on the gameboard that has already been shot', () => {
-        const computer = createPlayer()
-        const player = createPlayer()
-        computer.attack('A', 1, player)
-        expect(computer.attack('A', 1, player)).toBe('You have already hit this spot!')
+        computer.attack('I', 0, player)
+        expect(computer.attack('I', 0, player)).toBe('You have already hit this spot!')
     })
     test('if the user has a correctly intialized gameBoard', () => {
-        const player = createPlayer()
         const playerGameBoard = player.getBoard()
         expect(playerGameBoard).toEqual({
             sinkAllShips: playerGameBoard.sinkAllShips,
