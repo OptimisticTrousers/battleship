038343d4ebc3d2c9488af0c470e09de1ed0afb93
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-26 15:15:23 -0400

    Adding event listeners that mark the ship red when the user clicks on it

--- src/dom.js
+++ src/dom.js
@@ -1,35 +1,40 @@
-const checkIfCellHasShip = (event) => {
-    console.log('hiiii this works')
+const checkIfEnemyCellHasShip = (cell, column, row, event, getLocation) => {
+    const cellLocation = getLocation(column, row)
+    if (cellLocation.isShip) {
+        cell.classList.add('hit')
+        if (cellLocation.isSunk()) {
+            cell.classList.add('sunk')
+        }
+    }
 }
 
 export const addListenersToPlayerBoard = ({ getLocation }) => {
     const playerBoardArea = document.querySelector('div.player-board')
 
-    for (let i = 0; i < 10; i += 1) {
-        for (let j = 0; j < 10; j += 1) {
+    for (let column = 0; column < 10; column += 1) {
+        for (let row = 0; row < 10; row += 1) {
             const cell = playerBoardArea.querySelector(
-                `.cell:nth-child(${j * 10 + i + 1})`
+                `.cell:nth-child(${row * 10 + column + 1})`
             )
-            if (getLocation(j, i).isShip) {
+            if (getLocation(row, column).isShip) {
                 cell.classList.add('ship')
             }
         }
     }
 }
 
-export const addListenersToEnemyBoard = () => {
+export const addListenersToEnemyBoard = ({ getLocation }) => {
     const enemyBoardArea = document.querySelector('div.enemy-board')
 
-    for (let i = 0; i < 10; i += 1) {
-        for (let j = 0; j < 10; j += 1) {
+    for (let column = 0; column < 10; column += 1) {
+        for (let row = 0; row < 10; row += 1) {
             // Crediting formula to calculate nth-child using nested loop: https://stackoverflow.com/questions/8872662/math-to-determine-item-index-based-on-col-row-selection-in-grid
             const cell = enemyBoardArea.querySelector(
-                `.cell:nth-child(${j * 10 + i + 1})`
+                `.cell:nth-child(${row * 10 + column + 1})`
             )
-            cell.addEventListener('click', checkIfCellHasShip)
+            cell.addEventListener('click', (event) => {
+                checkIfEnemyCellHasShip(cell, column, row, event, getLocation)
+            })
         }
     }
 }
-
-addListenersToEnemyBoard()
-
--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import { addListenersToPlayerBoard, addListenersToEnemyBoard} from './dom'
 import createGameBoard from './gameboard/gameboard'
 
 const playerBoard = createGameBoard()
+const enemyBoard = createGameBoard()
+
 playerBoard.randomlyPlaceShips()
+enemyBoard.randomlyPlaceShips()
+
+addListenersToEnemyBoard(enemyBoard)
 
 addListenersToPlayerBoard(playerBoard)
 
